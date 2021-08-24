8aa994bd5a42313489244bdb5f58a98ef7ae66a5
beneko <behzadnekouei@gmail.com>
2021-08-24 09:57:41 +0200

    custom adapter created

--- app/src/main/java/com/afpa/androidlistview/MainActivity.java
+++ app/src/main/java/com/afpa/androidlistview/MainActivity.java
@@ -3,8 +3,11 @@ package com.afpa.androidlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import model.UserAccount;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,16 +18,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Récupération de listview
-        ListView listView = (ListView)findViewById(R.id.listView);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         //Création des users
         UserAccount francois = new UserAccount("François","admin");
         UserAccount germain = new UserAccount("Germain","user");
         UserAccount cedric = new UserAccount("Cédric","guest",false);
+        UserAccount manu = new UserAccount("Manu","guest");
+
         //Création de la liste
-                UserAccount[] users = new UserAccount[]{francois,germain,cedric};
+        List<UserAccount> users = new ArrayList<UserAccount>();
+        users.add(francois);
+        users.add(germain);
+        users.add(cedric);
+        users.add(manu);
+
         //Création de l'adapter
-                ArrayAdapter<UserAccount> arrayAdapter = new ArrayAdapter<UserAccount>(this, android.R.layout.simple_list_item_1,users);
+//                ArrayAdapter<UserAccount> arrayAdapter = new ArrayAdapter<UserAccount>(this, android.R.layout.simple_list_item_1,users);
         //Affectation de l'adapter
-                listView.setAdapter(arrayAdapter);
+//                listView.setAdapter(arrayAdapter);
+
+
+        // utilisation du customListAdapter sur listView
+        listView.setAdapter(new CustomListAdapter(users, this));
     }
 }
--- app/src/main/java/model/UserAccount.java
+++ app/src/main/java/model/UserAccount.java
@@ -1,25 +1,50 @@
 package model;
 
+import androidx.annotation.NonNull;
+
 public class UserAccount {
 
     private String userName;
     private String userType;
-    private Boolean active;
+    private boolean active;
 
     public UserAccount(String userName, String userType) {
         this.userName = userName;
         this.userType = userType;
+        this.active = true;
     }
 
-    public UserAccount() {
-    }
-
-    public UserAccount(String userName, String userType, Boolean active) {
+    public UserAccount(String userName, String userType, boolean active) {
         this.userName = userName;
         this.userType = userType;
         this.active = active;
     }
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @NonNull
     @Override
     public String toString(){
         return this.userName+"("+this.userType+")";
